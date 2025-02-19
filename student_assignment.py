05885a4df55ad92f62d79d97999ced24ee2bc23f
sammi_ms <sammi_ms@asus.com>
2025-02-19 16:24:44 +0800

    hw02_1

--- student_assignment.py
+++ student_assignment.py
@@ -7,9 +7,21 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    docs = loader.load()
+    print(f"pages: {len(docs)}")
+
+    spiltter = CharacterTextSplitter(chunk_overlap = 0)
+    chunks = spiltter.split_documents(docs)
+    # negative indices where -1 refers to the last element,
+    # -2 to the second-last, and so on
+    last_chunk = chunks[-1]
+    print(f"last chunk: {last_chunk.metadata}")
+
+    return last_chunk
 
 def hw02_2(q2_pdf):
     pass
 
-print("test")
+hw02_1(q1_pdf)
+hw02_2(q2_pdf)
