594cf5e134886c1c49e5cfb4e3ec43444aa05e6f
sammi_ms <sammi_ms@asus.com>
2025-02-19 17:13:37 +0800

    hw02_2

--- student_assignment.py
+++ student_assignment.py
@@ -20,8 +20,43 @@ def hw02_1(q1_pdf):
 
     return last_chunk
 
-def hw02_2(q2_pdf):
-    pass
+def hw02_2(q2_pdCf):
+    loader = PyPDFLoader(q2_pdf)
+    docs = loader.load()
+    print(f"pages: {len(docs)}")
+
+    # merge all docs to one plain text
+    plain_text = ""
+    for doc in docs:
+        plain_text = plain_text + doc.page_content + "\n"
+    print(f"plain_text: \n{plain_text}\n")
+
+    # step1: split to chpaters
+    chapter_separator = [r"(?:\n|^|\s*)第\s+(?:[一二三四五六七八九十百千萬]+|零)\s+章.*\n"]
+    chapter_splitter = RecursiveCharacterTextSplitter(
+        separators = chapter_separator,
+        chunk_size = 0,
+        chunk_overlap = 0,
+        is_separator_regex = True,
+        keep_separator = True)
+    chapters = chapter_splitter.split_text(plain_text)
+    print(f"chapter count: {len(chapters)}")
+
+    # step2: split chapters to article, and collect chapter and article in one chunk
+    chapter_article_chunks = []
+    article_separator = [r"(?:\n|^)第\s+[0-9]+(?:-[0-9]+)?\s+條.*\n"]
+    article_splitter = RecursiveCharacterTextSplitter(
+        separators = article_separator,
+        chunk_size = 0,
+        chunk_overlap = 0,
+        is_separator_regex = True,
+        keep_separator = True)
+    for chapter in chapters:
+        articles = article_splitter.split_text(chapter)
+        chapter_article_chunks.extend(articles)
+
+    print(f"result chunk count: {len(chapter_article_chunks)}")
+
 
 hw02_1(q1_pdf)
 hw02_2(q2_pdf)
